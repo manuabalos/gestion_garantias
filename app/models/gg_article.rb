6b4c0031e15ce222d41020245bfcfcdcab1816f0
manuabalos <manu.abalos@gmail.com>
2015-12-22 11:37:36 +0100

    Validaciones y estilos.

--- app/models/gg_article.rb
+++ app/models/gg_article.rb
@@ -5,17 +5,20 @@ class GgArticle < ActiveRecord::Base
   has_many :gg_contacts, :dependent => :destroy
   accepts_nested_attributes_for :gg_contacts
 
+  validates :code_article, :numericality => { :message => l(:"article.error.validation_code_article_number")}
+  validates :guarantee, :numericality => { :message => l(:"article.error.validation_guarantee_number")}
+  validates :ans, :numericality => { :message => l(:"article.error.validation_ans_number")}
+  
   validate :code_article_is_blank
   validate :name_provider_is_blank
   validate :guarantee_is_blank
-  # validate :guarantee_is_number
   validate :ans_is_blank
-  # validate :ans_is_number
+  validate :email_regexp
+  validate :phone_regexp
 
   # Genera mensaje de error
   def get_error_message
     error_msg = ""
-    
     self.errors.full_messages.each do |msg|
       if error_msg != ""
         error_msg << "<br>"
@@ -26,27 +29,49 @@ class GgArticle < ActiveRecord::Base
     error_msg
   end
 
+  private
+  # Valida que el código de articulo no se encuentre vacío.
   def code_article_is_blank
- 	errors.add :base, l(:"article.error.validation_code_article_blank") if self.code_article.blank?
+ 	  errors.add :base, l(:"article.error.validation_code_article_blank") if self.code_article.blank?
   end
 
+  # Valida que el nombre del proveedor no se encuentre vacío.
   def name_provider_is_blank
- 	errors.add :base, l(:"article.error.validation_name_provider_blank") if self.name_provider.blank?
+ 	  errors.add :base, l(:"article.error.validation_name_provider_blank") if self.name_provider.blank?
   end
 
+  # Valida que la garantía no se encuentre vacío.
   def guarantee_is_blank
- 	errors.add :base, l(:"article.error.validation_guarantee_blank") if self.guarantee.blank?
+ 	  errors.add :base, l(:"article.error.validation_guarantee_blank") if self.guarantee.blank?
   end
 
+  # Valida que el ANS no se encuentre vacío.
   def ans_is_blank
- 	errors.add :base, l(:"article.error.validation_ans_blank") if self.ans.blank?
+    errors.add :base, l(:"article.error.validation_ans_blank") if self.ans.blank?
   end
 
- #  def guarantee_is_number
- #  	errors.add :base, l(:"article.error.validation_guarantee_number") if self.guarantee.is_a? Integer
- #  end
+  # Valida el email tenga la expresión regular correcta.
+  def email_regexp
+    validation_email = false
 
- #  def ans_is_number
-	# errors.add :base, l(:"article.error.validation_ans_number") if self.ans.is_a? Integer
- #  end
+    (0..2).each do |i|
+      if validation_email == false && !self.gg_contacts[i].email.blank? && /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/.match(self.gg_contacts[i].email).nil?
+          errors.add :base, l(:"contact.error.validation_email_regexp") 
+          validation_email = true
+      end
+    end
+  end
+
+  # Valida el número de teléfono tenga la expresión regular correcta:
+  def phone_regexp
+    validation_phone = false
+
+    (0..2).each do |i|
+      if validation_phone == false && !self.gg_contacts[i].phone.blank? && /^[0-9]{2,3}-? ?[0-9]{2} ?[0-9]{2} ?[0-9]{2}$/.match(self.gg_contacts[i].phone).nil?
+         errors.add :base, l(:"contact.error.validation_phone_regexp") 
+         validation_phone = true
+      end
+    end
+  end
+
 end
--- app/models/gg_file.rb
+++ app/models/gg_file.rb
@@ -2,8 +2,10 @@
   unloadable
 
   has_many :gg_articles, :dependent => :destroy
-  #validates :identity_file, :presence => { :message => "El identidicador del expediente no puede estar vacío." }
+
   validate :identity_file_is_blank
+  validate :identity_file_is_number
+  validate :identity_file_is_unique
   validate :code_file_is_blank
 
   # Genera mensaje de error
@@ -24,6 +26,16 @@
   # Valida que el campo del numero de identidad del expediente no se encuentre vacio.
   def identity_file_is_blank
   	 errors.add :base, l(:"file.error.validation_indentity_file_blank") if self.identity_file.blank?
+  end
+
+  # Valida que el identificador del expediente es un número
+  def identity_file_is_number
+    errors.add :base, l(:"file.error.validation_identity_file_number") if (self.identity_file.is_a? Integer) == false
+  end
+
+  # Valida que el identificador de expediente sea un número único.
+  def identity_file_is_unique
+    errors.add :base, l(:"file.error.validation_identity_file_unique") if GgFile.where("identity_file = ?", self.identity_file).present?
   end
 
   # Valida que el campo del código del expediente no este vacio.
