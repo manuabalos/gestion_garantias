8fffd1ebace462f74270ea9814321ea0c583f869
manuabalos <manu.abalos@gmail.com>
2015-12-22 12:12:05 +0100

    Mejoradas y añadidas algunas traducciones.

--- app/models/gg_contact.rb
+++ app/models/gg_contact.rb
@@ -3,17 +3,4 @@ class GgContact < ActiveRecord::Base
   
   belongs_to :gg_article
 
-  # Genera mensaje de error
-  def get_error_message
-    error_msg = ""
-    
-    self.errors.full_messages.each do |msg|
-      if error_msg != ""
-        error_msg << "<br>"
-      end
-      error_msg << msg
-    end
-
-    error_msg
-  end
 end
